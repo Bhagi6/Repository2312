711ffb0f9d0bb6bf3905f5e51190432295147236
Bhargavi Evuri <134255253+Bhagi6@users.noreply.github.com>
2024-04-25 22:33:00 +0530

    Update App.js
    add the second details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1>This is Added to the Github Remote repository</h1>
+    <h1>This added from the github not the loacl repository but the remote repository</h1>
     </div>
   );
 }
